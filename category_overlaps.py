f021198a90ca2c9b6cc0f36f46130f1a6807a3dd
Kurt Rose <doublereedkurt@gmail.com>
2012-01-22 10:42:05 -0800

    converted category population count to new type

--- category_overlaps.py
+++ category_overlaps.py
@@ -1,60 +1,25 @@
+import itertools
+from collections import defaultdict
 
-def process_overlaps(category_article_dict, depth=2):
-    '''
-    figures out how much categories overlap
-    input:
-    category_article_dict = {category id : iterable of article ids}
-    depth = how large sets to build
-    
-    output:
-    [{set of one category   id : set of article ids in category},
-     {set of two category   ids: set of article ids in both categories},
-     {set of three category ids: set of article ids in all three categories]}
-     ...
-    ]
-    
-    O(n) behavior is depth * n**2
-    
-    >>> category_overlaps.process_overlaps({"a":[1,2,3], "b":[1,2], "c":[3,4,5]})
-[{frozenset(['c']): [3, 4, 5], frozenset(['b']): [1, 2], frozenset(['a']): [1, 2
-, 3]}, {}]
-    '''
-    category_article_set_dict = dict([(c, set(category_article_dict[c]))
-                                        for c in category_article_dict])
-    sofar = [dict([(frozenset([c]), set(category_article_dict[c]))
-                        for c in category_article_dict])]
-    return _process_overlaps2(category_article_set_dict, depth-1, sofar)
+'''
+>>> category_overlaps.article_category_dict2category_set_counts(
+{'cats':['mammals', 'felines', 'pets'], 'dogs':['mammals', 'canines', 'pets'],
+'goldfish':['fish', 'pets']})
 
-def _process_overlaps2(cat_dict, depth, sofar):
-    'recursive helper for process_overlaps'
-    if depth == 0:
-        return sofar
-    
-    prev = sofar[-1]
-    next = {}
-        
-    for left_category in cat_dict:
-        left = cat_dict[left_category]
-        for right_category_set in prev:
-            if left_category not in right_category_set:
-                right  = prev[right_category_set]
-                #union of key sets
-                newkey = frozenset([left_category]) | right_category_set
-                #intersection of value sets
-                newval = left & right
-                if len(newval) > 0: #the vast majority of overlaps will be empty
-                    next[newkey] = newval
-            
-    return _process_overlaps2(cat_dict, depth-1, sofar+[next])
-            
+defaultdict(<type 'int'>, {('mammals', 'felines', 'pets'): 1,
+('fish', 'pets'): 1, ('pets',): 3, ('mammals', 'pets'): 2, ('mammals',): 2,
+('fish',): 1, ('felines', 'pets'): 1, ('canines',): 1,
+('mammals', 'canines', 'pets'): 1, ('mammals', 'felines'): 1, ('felines',): 1,
+('canines', 'pets'): 1, ('mammals', 'canines'):1})
+'''
 
-def compact(category_overlaps):
-    'compacts the overlaps data to just counts, as needed to make a recommendation'
-    compact = {}
-    for article_dict in category_overlaps: #iterating over list
-        for category_set in article_dict: #iterating over dict
-            compact[category_set] = len(article_dict[category_set])
-    return compact
+def article_category_dict2category_set_counts(article_category_dict):
+    category_set_counts = defaultdict(int)
+    for category_list in article_category_dict.values():
+        for i in range(len(category_list)):
+            for subset in itertools.combinations(category_list, i+1):
+                category_set_counts[subset] += 1
+    return category_set_counts
 
 class Recommendation(object):
     def __init__(self, confidence, category, reason):
@@ -86,5 +51,3 @@ def make_recommendations(category_list, category_set_counts):
                         category   = category
                     )
     return sorted(results.values(), key=lambda a: a.confidence)
-    
-    
